9b256ffd2fc7ee0b9ece57db749ae8457615f5d3
laura.deoliveira <laura.deoliveira@ambevtech.com.br>
2021-08-18 18:06:12 -0300

    javascript - ex 2 - twoFer

---
+++ teams/start-tech/javascript/two-fer/two-fer.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+/*export const twoFer = (name) => {
+  if (name == undefined){
+    return "One for you, one for me."
+  } 
+    else if (name == 'Alice') { 
+        return "One for Alice, one for me."
+      } 
+        else {
+          return "One for Bob, one for me.";
+        }
+}; */
+
+export const twoFer = (name) => {
+  if (typeof name === 'undefined') {
+    return "One for you, one for me."
+  } else 
+    return `One for ${name}, one for me.`
+}
