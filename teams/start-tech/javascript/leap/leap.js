14a8c0d433adbf5ac6fe01b4a41a381e5a21c1c4
laura.deoliveira <laura.deoliveira@ambevtech.com.br>
2021-08-20 09:29:45 -0300

    javascript - ex 3 - leap

--- teams/start-tech/javascript/leap/leap.js
+++ teams/start-tech/javascript/leap/leap.js
@@ -16,11 +16,11 @@ export const isLeap = (anoAVerificar) => {
   return ehBissexto;
 };
 
-export const isLeap = (ano) => { 
+/*export const isLeap = (ano) => { 
   if ( (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0) ) {
      return true 
   } 
   else { 
     return false 
   } 
-};
+};*/
